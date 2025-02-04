c9ab61e560d73fad364189cd755933d367a7a6d1
whoru000104 <ssimoorancer@gmail.com>
2025-02-04 15:41:15 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import ResultLastPage from './pages/ResultLastPage';
+import ProfilePage from './pages/ProfilePage';
+import LoginPage from './pages/LoginPage'
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/ResultLastPage" element={<ResultLastPage/>} />
+        <Route path="/ProfilePage" element={<ProfilePage/>} />
+        <Route path="/LoginPage" element={<LoginPage/>} />
+
+      </Routes>
+    
+  );
+}
+
+export default App;
